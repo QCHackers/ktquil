c1ac04c671ce8e8444398e40df6fe170a723213e
vtomole <vtomole@iastate.edu>
2018-07-14 15:48:07 -0500

    code format

--- src/main/kotlin/ktquil/EPR.kt
+++ src/main/kotlin/ktquil/EPR.kt
@@ -7,21 +7,21 @@ import java.util.Arrays
 import java.util.ArrayList
 
 fun H(qubit : Int) : String{
-    return g.H(qubit)
+	return g.H(qubit)
 }
 
 fun X(qubit : Int) : String{
-    return g.H(qubit)
+	return g.H(qubit)
 }
 
 fun CNOT(qubit0 : Int, qubit1 : Int) : String{
-    return g.CNOT(qubit0, qubit1)
+	return g.CNOT(qubit0, qubit1)
 }
 
 fun wavefunction(program : Program) : String {
-    return QVM.wavefunction(program,listOf(1, 2, 3))
+	return QVM.wavefunction(program,listOf(1, 2, 3))
 }
 
 fun main(args: Array<String>) {
-   println(wavefunction(Program(H(0), CNOT(0, 1))))
+	println(wavefunction(Program(H(0), CNOT(0, 1))))
 }
--- src/test/kotlin/ktquil/tests.kt
+++ src/test/kotlin/ktquil/tests.kt
@@ -8,7 +8,7 @@ import kotlin.test.assertEquals
 import org.junit.Test
 
 class TestSource {
-    @Test fun f() {
-        assertEquals("0.71|00> + 0.71|11>", wavefunction(Program(H(0), CNOT(0, 1))))
-    }
+	@Test fun f() {
+		assertEquals("0.71|00> + 0.71|11>", wavefunction(Program(H(0), CNOT(0, 1))))
+	}
 }
